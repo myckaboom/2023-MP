69d8cde7592ce6afa21fdd94f3dc27241f996f44
SVALE <118879825+myckaboom@users.noreply.github.com>
2023-02-18 14:07:23 +0300

    tuyfiuytcjgfc

--- 429/kalininS.py
+++ 429/kalininS.py
@@ -1,3 +1,6 @@
+import datetime as dt
+import que
+
 my_data = ("Калинин Святослав Ильич", 15, 11, 2003)
 attestate = {
              "физра":4,
@@ -9,5 +12,25 @@ attestate = {
 family_names=["Илья","Лариса", "Арина", "Святослав"]
 kiwa_name="Еж"
              
-for i in range(len(attestate)):
-   
+mean_mark=sum(attestate.value())/len(attestate)
+print(mean_mark) 
+
+unique_names=[]
+for name in family_names:
+    if name in unique_names
+        continue
+    unique_names.append(name)
+print(unique_names)
+print(set(family_names)) #(f_names[0], f_names[1]
+
+print(*bytearray(kiwa_name, 'utf-8'))
+
+today=datetime.datetime.today()
+my_bday=datetime.datetime(day=my_data[1]), month=my_data[2],year=my_date[3]))
+print((today-my_bday).days)
+
+stuff=Queue()
+while i !='q'
+    i=int(input())
+    stuff.add(i)
+print|(stuff)
